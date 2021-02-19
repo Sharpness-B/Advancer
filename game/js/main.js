1430445ebad42b183dd00e9363c0c9585ffd0557
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2021-02-19 10:35:22 +0100

    Update main.js

--- game/js/main.js
+++ game/js/main.js
@@ -1,3 +1,9 @@
-function pipeline()
-{
+let objects = {};
+let data = {point: [1,2,3]};
+
+function pipeline() {
+    updateObjects("db/retriever.php", data);
+    console.log(objects);
 }
+
+setInterval(pipeline, 50);
