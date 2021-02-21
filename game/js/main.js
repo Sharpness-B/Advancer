aeb4b4ea9bf88e0d1d0ae86a57bb64a597743604
Vaakir <77720551+Vaakir@users.noreply.github.com>
2021-02-21 12:39:00 +0100

    Update main.js

--- game/js/main.js
+++ game/js/main.js
@@ -17,3 +17,44 @@ function pipeline() {
 }
 
 setInterval(pipeline, 500);  
+
+/*      Updates canvas size on rezise    */
+let cnv = document.getElementsByTagName("canvas")[0];
+function fit_to_screen(){
+    cnv.width = innerWidth-5;
+    cnv.height = innerHeight-5;
+} fit_to_screen();
+window.addEventListener('resize', fit_to_screen);
+
+/* Keyinputs */
+let loc = document.location.href.split("/");
+if (loc[loc.length-1].toString() == "singleplayer.html" || loc[loc.length-1].toString() == "multiplayer.html") {
+    document.addEventListener('keyup', function(){
+        let key = event.key;
+        if (key == "Escape") {
+            let ingame_menu = document.getElementById("main_menu");
+            if (ingame_menu.style.display != "block") ingame_menu.style.display = "block";
+            else {ingame_menu.style.display = "none";}
+        }
+    });
+}
+
+/* MUSIC */
+function readCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') { c = c.substring(1,c.length); }
+        if (c.indexOf(nameEQ) == 0) return parseFloat(c.substring(nameEQ.length,c.length));
+    }
+    return null;
+}
+
+let audioElement = document.createElement('audio');
+if (loc[loc.length-1].toString() == "index.html") audioElement.setAttribute('src', 'assets/music1.mp3');
+else {audioElement.setAttribute('src', '../assets/music1.mp3'); }
+audioElement.load();
+audioElement.play();
+c = readCookie("music");
+if (c) audioElement.currentTime = c;
