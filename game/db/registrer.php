444d06a2bf3f8a6df90ca284b83cc5974c4f21b2
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-02-24 22:53:41 +0100

    minor update: added a balance row, and default values to the users table

--- game/db/registrer.php
+++ game/db/registrer.php
@@ -10,7 +10,7 @@
     // hvis ikke resultat fra db <=> hvis ikke ID i db
     if (!$row) {
         // registrer ny bruker
-        $sql = "INSERT INTO users VALUES(\"$userID\", 1, 1, 1, 1, 1);";
+        $sql = "INSERT INTO users VALUES(\"$userID\");";
         $result = mysqli_query($connection, $sql);
  
         // returner upgrades
