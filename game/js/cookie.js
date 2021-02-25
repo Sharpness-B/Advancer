278d2ec246e95e50d640bc11295be897f2c6967a
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-02-25 10:32:22 +0100

    minor change in the reading of cookies

--- game/js/cookie.js
+++ game/js/cookie.js
@@ -5,7 +5,10 @@ function readCookie(name) {
     for(let i=0;i < ca.length;i++) {
         let c = ca[i];
         while (c.charAt(0)==' ') { c = c.substring(1,c.length); }
-        if (c.indexOf(nameEQ) == 0) return parseFloat(c.substring(nameEQ.length,c.length));
+
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
     }
     return null;
 }
