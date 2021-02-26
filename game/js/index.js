809c9baed549d2040f5c69e2aa40174647b3e817
Vaakir <77720551+Vaakir@users.noreply.github.com>
2021-02-26 11:29:56 +0100

    Add files via upload

---
+++ game/js/index.js
@@ -0,0 +1,32 @@
+document.getElementById("upgr_body").style.display = "none";
+
+document.getElementsByClassName("button")[0].onmouseover = function(){sound();}
+document.getElementsByClassName("button")[1].onmouseover = function(){sound();}
+document.getElementsByClassName("button")[2].onmouseover = function(){sound();}
+
+document.getElementById("upgrades").onclick = function() {
+    document.getElementById("upgr_body").style.display = "flex";
+    document.getElementById("main_menu").style.display = "none";
+}
+document.getElementById("multiplr").onclick = function() {
+    musicCheck();
+    location.href = "multiplayer.html"; 
+}
+document.getElementById("grinding").onclick = function() {
+    musicCheck();
+    location.href = "singleplayer.html"; 
+}
+document.getElementById("go_back").onmouseover = function(){sound();}
+document.getElementById("go_back").onclick = function() {
+    document.getElementById("upgr_body").style.display = "none";
+    document.getElementById("main_menu").style.display = "block";
+}
+
+// Button sound effect
+let audioButton = document.createElement('audio');
+audioButton.setAttribute('src', 'assets/button.mp3');
+audioButton.load();
+function sound(){
+    audioButton.currentTime=0;
+    audioButton.play();
+}
