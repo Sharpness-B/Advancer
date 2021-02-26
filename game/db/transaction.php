c2c65c258cc928f13a286aca3c19b9f998b1d8fe
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-02-26 17:09:19 +0100

    programmed backend code for the upgrade system

---
+++ game/db/transaction.php
@@ -0,0 +1,52 @@
+<?php
+    function price($level) {
+        return $level*10;
+    }
+
+    require("connection.php");
+    $userID  = $_GET["userID"];
+    $upgrade = $_GET["upgrade"];
+
+
+
+    // Hente ut verdier
+    $sql = "SELECT * FROM users WHERE id=$userID;";
+    $result = mysqli_query($connection, $sql);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $values = [
+            "balance" => $row["balance"],
+            "armour"  => $row["armour"],
+            "speed"   => $row["speed"],
+            "laser"   => $row["laser"],
+            "missile" => $row["missile"],
+            "energy"  => $row["energy"]
+        ];
+    }
+
+
+
+    // har brukeren nok penger, og er nivået under max?
+    if ($values["balance"] >= price($values[$upgrade]) && $values[$upgrade]<5) {
+        // gjennomføre transaksjon
+        $sql = "UPDATE users 
+                SET balance = balance-".price($values[$upgrade]).",
+                    $upgrade = $upgrade+1
+                WHERE id=$userID;";
+
+        $result = mysqli_query($connection, $sql);
+
+        $values[$upgrade] ++;
+    }
+
+
+
+    // returner nytt objekt
+    echo "{    
+        \"armour\":  ".$values["armour"].",
+        \"speed\":   ".$values["speed"].",
+        \"laser\":   ".$values["laser"].",
+        \"missile\": ".$values["missile"].",
+        \"energy\":  ".$values["energy"]."
+    }";
+?>
