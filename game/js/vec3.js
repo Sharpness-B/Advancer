4314146cda2a01a0cc5d0a6e1f0becf64fbc48db
Vaakir <kiranvaagen@gmail.com>
2021-03-24 08:34:24 +0100

    no change

--- game/js/vec3.js
+++ game/js/vec3.js
@@ -57,3 +57,66 @@ class vec3
         this.z /= len;
     }
 }
+
+
+
+
+var pressKey = function(event) {
+    
+}
+const unpressKey = function(event) {
+    
+}
+var screenBoxCX = window.innerWidth / 2;
+var screenBoxCY = window.innerHeight / 2;
+
+const getMouseInGeneral = function(event) {
+    screenBoxCX = window.innerWidth / 2;
+    screenBoxCY = window.innerHeight / 2;
+    var mouseClientX = event.clientX - screenBoxCX;
+    var mouseClientY = event.clientY - screenBoxCY;
+
+    
+}
+
+
+const getMouseDown = function(mouse) {
+    //var mousepress = 0;
+    //document.getElementById("click").innerHTML = mouse.button;
+    switch (mouse.button) {
+        case 1:
+            mousepress[1] = 1
+            break;
+        case 0:
+            mousepress[0] = 1
+            break;
+        case 2:
+            mousepress[2] = 1
+            break;
+    }
+}
+
+const getMouseUp = function(mouse) {
+        switch (mouse.button) {
+        case 1:
+            mousepress[1] = 0
+            break;
+        case 0:
+            mousepress[0] = 0
+            break;
+        case 2:
+            mousepress[2] = 0
+            break;
+    }
+}
+
+
+function sendMouse(x, y) {
+
+}
+
+document.addEventListener('mousemove', getMouseInGeneral);
+document.addEventListener('keydown', pressKey);
+document.addEventListener('keyup', unpressKey);
+document.addEventListener('mousedown', getMouseDown);
+document.addEventListener('mouseup', getMouseUp);
