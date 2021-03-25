5f43234bd71deb99258e7f86d5d168b8a026b398
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-03-25 16:49:25 +0100

    altered registrer mechanics such that accounts still are recovered if the fingerprint changes

--- game/db/registrer.php
+++ game/db/registrer.php
@@ -4,18 +4,30 @@
     $fingerprint = $_GET["fingerprint"];
 
 
-    
-    // sjekk om bruker finnes fra før av
-    if (!$userID) { // hvis 0
-        // registrer ny bruker
-        $sql = "INSERT INTO users (fingerprint) VALUES(\"$fingerprint\");";
-        $result = mysqli_query($connection, $sql);
 
-        // hent ut id
-        $sql = "SELECT * FROM users WHERE fingerprint=\"$fingerprint\" ORDER BY id ASC LIMIT 1;"; // fingerprint er kandidatnøkkel
+    // hvis 0 <=> hvis cookien ikke finnes
+    if (!$userID) {
+        // hvis fingerprinten ikke finnes i db
+        $sql = "SELECT * FROM users WHERE fingerprint=\"$fingerprint\";";
+        $result = mysqli_query($connection, $sql);
+        $cnt = mysqli_num_rows($result);
+
+        if (!$cnt) {
+            // registrer ny bruker
+            $sql = "INSERT INTO users (fingerprint) VALUES(\"$fingerprint\");";
+            $result = mysqli_query($connection, $sql);
+        }
+
+        // hent ut id og upgrades
+        $sql = "SELECT * FROM users WHERE fingerprint=\"$fingerprint\";"; // fingerprint er kandidatnøkkel
     }
 
     else {
+        // oppdatere fingerprint
+        $sql = "UPDATE users SET fingerprint=\"$fingerprint\" WHERE id=$userID;";
+        $result = mysqli_query($connection, $sql);
+
+        // hent ut upgrades
         $sql = "SELECT * FROM users WHERE id=$userID;";
     }
 
--- game/js/userIdentifier.js
+++ game/js/userIdentifier.js
@@ -6,34 +6,30 @@ let xmlhttpUserID;
 
 function login() {
     console.log("logging in")
+
     // hvis cookie
     if (cookie.readCookie("userID")) {
         userID = cookie.readCookie("userID");   
         cookie.eraseCookie("userID");
-        registrer(userID, null);
     }
 
     // hvis ikke
     else {
         userID = 0;
-
-        FingerprintJS.load().then(fp => {
-            fp.get().then(result => {
-                fingerprint = result.visitorId;
-                registrer(userID, fingerprint)
-            });
-        });
     }
+
+    FingerprintJS.load().then(fp => {
+        fp.get().then(result => {
+            fingerprint = result.visitorId;
+            DBstoreUserID_getUpgrades("db/registrer.php", userID, fingerprint);
+        });
+    });
 }
 
 // registrer bruker i DB hvis ny. hent ut nivå på oppgraderinger, og lag en cookie til slutt
-function registrer(userID, fingerprint) { 
-    DBstoreUserID_getUpgrades("db/registrer.php", userID, fingerprint); 
-}
-
 function DBstoreUserID_getUpgrades(url, userID, fingerprint) {
     let params = "userID=" + userID +"&fingerprint=" + fingerprint;
-    
+
     xmlhttpUserID = new XMLHttpRequest();
     xmlhttpUserID.onreadystatechange = upgradesResponse;
     xmlhttpUserID.open("GET", url+"?"+params, true);
@@ -63,5 +59,5 @@ function upgradesResponse(){
 
 // <script defer src="js/userIdentifier.js"></script>
 // <script defer async src="https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js"
-//     onload="initFingerprintJS()">
+//     onload="login()">
 // </script>
