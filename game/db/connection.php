48db21cfdce91d7eb18b8a4298a2428f101ee036
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-04-07 17:32:24 +0200

    fixed file inclusion

--- game/db/connection.php
+++ game/db/connection.php
@@ -5,10 +5,10 @@
     $password   = "";
     $dbname     = "advancer";
 
-    // $servername = "mysql.elev.stolav.it";
-    // $username = "stolav_advancer";
-    // $password = "";
-    // $dbname = "stolav_advancer";
+    $servername = "mysql.elev.stolav.it";
+    $username = "stolav_advancer";
+    $password = "LRGH?v*C";
+    $dbname = "stolav_advancer";
     
     // the connection to db
     $connection = mysqli_connect(
