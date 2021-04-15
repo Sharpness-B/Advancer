e109326fb2574cff97dff23e1af2e576ebbcc97e
BendikSkarpnes <bendik.skarpnes@gmail.com>
2021-04-15 10:58:51 +0200

    multi

--- game/db/multiplayer/server.js
+++ game/db/multiplayer/server.js
@@ -1,5 +1,7 @@
-const io = require("socket.io")(); 
-
+let app = require("express")();
+let server = require("http").Server(app);
+let io = require("socket.io")(server, {origins:["*:*"]});
+// io.set("origins", "*:*");
 
 io.on("connection", client => {
     client.emit("init", {data:"hello world"});
--- game/js/multiplayerFrontend.js
+++ game/js/multiplayerFrontend.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:8080");
+const socket = io("http://127.0.0.1:8080/");
 
 socket.on("init", handleInit);
 
